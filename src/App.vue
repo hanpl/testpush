4c935f029d467f99bfa72dc69f1645e5aca55837
hanpl <phamlahan@gmail.com>
2023-09-06 13:25:17 +0700

    đ

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,7 @@
             <span class="text-lg text-gray-600">International Terminal</span>
          </div>
       </div>
-      <div class="flex items-center space-x-2">
+      <!-- <div class="flex items-center space-x-2">
          <button type="button" class="inline-flex items-center justify-center rounded-lg border h-10 w-10 transition duration-500 ease-in-out text-gray-500 hover:bg-gray-300 focus:outline-none">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-6 w-6">
                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
@@ -35,7 +35,7 @@
                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"></path>
             </svg>
          </button>
-      </div>
+      </div> -->
    </div>
    <div v-bind:class="{ishiden : active}">
         <div style="text-align: center;">
