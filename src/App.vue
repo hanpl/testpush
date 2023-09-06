5bcf536f6f68c4a95ce9e5fce617520d344cb850
hanpl <phamlahan@gmail.com>
2023-09-06 15:45:15 +0700

    dd

--- src/App.vue
+++ src/App.vue
@@ -57,7 +57,7 @@
                   </span>
                </div> -->
             </div>
-            <img src="https://images.unsplash.com/photo-1549078642-b2ba4bda0cdb?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144" alt="My profile" class="w-6 h-6 rounded-full order-1">
+            <img src="AHT.jpg" alt="My profile" class="w-6 h-6 rounded-full order-1">
          </div>
       </div>
       <div class="chat-message" v-else>
@@ -65,7 +65,7 @@
             <div class="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-1 items-end">
                <div><span class="px-4 py-2 rounded-lg inline-block rounded-br-none bg-blue-600 text-white ">{{ message.message }}</span></div>
             </div>
-            <img src="https://images.unsplash.com/photo-1590031905470-a1a1feacbb0b?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144" alt="My profile" class="w-6 h-6 rounded-full order-2">
+            <img src="Banh.jpg" alt="My profile" class="w-6 h-6 rounded-full order-2">
          </div>
       </div>
     </div>
